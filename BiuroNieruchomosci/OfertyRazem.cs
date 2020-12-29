a14fe8c44b0ae46943f9bdffc24adadbd031548c
klaudiapajor@student.agh.edu.pl <klaudiapajor@student.agh.edu.pl>
2020-12-29 20:08:46 +0100

    K

--- BiuroNieruchomosci/OfertyRazem.cs
+++ BiuroNieruchomosci/OfertyRazem.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace BiuroNieruchomosci
 {
-    public class OfertyRazem : ArchiwumOfert
+    public class OfertyRazem : IOferuje
     {
         List<Oferta> _listaOfert;
 
