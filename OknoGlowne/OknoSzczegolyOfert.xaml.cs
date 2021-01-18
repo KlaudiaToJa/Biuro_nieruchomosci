d6f2f64e0fb570534c6ab7a3be204a2c3b397915
KlaudiaToJa <74858427+KlaudiaToJa@users.noreply.github.com>
2021-01-18 21:38:06 +0100

    K

---
+++ OknoGlowne/OknoSzczegolyOfert.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using BiuroNieruchomosci;
+
+namespace OknoGlowne
+{
+    /// <summary>
+    /// Logika interakcji dla klasy OknoSzczegolyOfert.xaml
+    /// </summary>
+    public partial class OknoSzczegolyOfert : Window
+    {
+        Oferta _oferta = new Oferta();
+
+        public OknoSzczegolyOfert()
+        {
+            InitializeComponent();
+        }
+
+        public OknoSzczegolyOfert(Oferta o) : this()
+        {
+            _oferta = o;
+            txtBoxDataWystawienia.Text = _oferta.DataWystawienia.ToString("dd-MM-yyyy");
+            txtBoxIdOferty.Text = _oferta.IdOferty;
+            if (_oferta.CzyAktywna)
+            {
+                txtBoxStatus.Text = "Aktywna";
+            }
+            else
+            {
+                txtBoxStatus.Text = "Zakonczona";
+            }
+        }
+    }
+}
